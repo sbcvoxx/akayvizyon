42075f87480e12f9a7a838d85da88fc7712773e7
Stephen Corrigan <122071696+sbcvoxx@users.noreply.github.com>
2024-09-16 09:56:08 +0300

    Create app.js
    Initial commit

---
+++ css/js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $("#slideshow__box > div:gt(0)").hide();
+
+    setInterval(function() {
+    $('#slideshow__box > div:first')
+        .fadeOut(1000)
+        .next()
+        .fadeIn(1000)
+        .end()
+        .appendTo('#slideshow__box');
+    }, 1500);
+});
