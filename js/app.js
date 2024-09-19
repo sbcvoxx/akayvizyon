5edaf16ec1763026fee49c6200281e0f82fa91b0
Stephen Corrigan <122071696+sbcvoxx@users.noreply.github.com>
2024-09-20 02:11:29 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,12 +4,15 @@ $(document).ready(function() {
 
     setInterval(function() {
     $('#slideshow__box > div:first')
+        .stop()
         .fadeOut(1000)
         .next()
         .fadeIn(1000)
         .end()
         .appendTo('#slideshow__box');
     }, 1500);
-    
+
+
     $("#slideshow__box").show();
+
 });
